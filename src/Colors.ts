aea571c16713e689def0572070bb4e5003090571
fellypsantos <fellypsantos2011@gmail.com>
2023-06-29 15:23:01 -0300

    setup app colors

---
+++ src/Colors.ts
@@ -0,0 +1,15 @@
+interface IColors {
+    Primary: string;
+    Secondary: string;
+    Contrast: string;
+    TextPrimary: string;
+}
+
+const Colors: IColors = {
+    Primary: '#7159c1',
+    Secondary: '#984eb8',
+    Contrast: '#F4EBF7',
+    TextPrimary: '#545454'
+}
+
+export default Colors;
