25abf559dc6feadf3e261d6ba0c86b05c95559af
fellypsantos <fellypsantos2011@gmail.com>
2023-07-07 13:05:35 -0300

    debug codes in Home tab

--- src/Tabs/Home/index.tsx
+++ src/Tabs/Home/index.tsx
@@ -1,4 +1,3 @@
-import { Linking, Text } from "react-native";
 import ScrollViewContainer from "../../components/ScrollViewContainer";
 
 import { BottomContainer, ListContainer, ListItemTouchable, TopContainer } from "./styles";
@@ -10,7 +9,18 @@ import NoFoodRegistry from "../../components/NoFoodRegistry";
 import CallToAction from "../../components/CallToAction";
 import FoodRegistryListItem from "../../components/FoodRegistryListItem";
 
-export default function () {
+import { useQuery, useRealm } from "../../databases";
+import { FoodHistory } from "../../databases/schema/FoodHistory";
+import { Linking } from "react-native";
+import { useContext } from "react";
+import { TranslationContext } from "../../contexts/TranslationContext";
+
+export default function Home() {
+
+  const { Translator } = useContext(TranslationContext);
+
+  const foodHistory = useQuery(FoodHistory);
+  const realm = useRealm();
 
   const theFoodHistory = [{ id: 1 }, { id: 2 }];
 
@@ -37,10 +47,39 @@ export default function () {
       </BottomContainer>
 
       <CallToAction
+        title="Mais Aplicativos"
+        description="Descrição sobre acessar mais apps."
         handleOnPress={() => {
           Linking.openURL('https://play.google.com/store/apps/dev?id=4983605265674024761');
         }}
       />
+
+      <CallToAction title="DEV" description="Botão de testes" handleOnPress={() => {
+        console.log('Testando realm db!');
+
+        const response = foodHistory.toJSON();
+
+        console.log('response = ', response);
+
+      }} />
+
+      <CallToAction title={Translator('Welcome')} description="Botão de ação para manipular a database." handleOnPress={() => {
+        console.log('testando context');
+
+        // realm.write(() => {
+        //   // realm.create('food_history', FoodHistory.generate({
+        //   //   name: 'Refeição Teste',
+        //   //   kcal: 321,
+        //   //   category_level: 2,
+        //   //   datetime: new Date(),
+        //   // }));
+
+        //   realm.deleteAll();
+
+        //   console.log('table is clean');
+        // });
+
+      }} />
     </ScrollViewContainer>
   );
 }
