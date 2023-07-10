5229df076b10d19ba83a62ef8136d4fb22a96d9b
fellypsantos <fellypsantos2011@gmail.com>
2023-07-09 21:05:46 -0300

    added splash screen loading

--- src/Screens/SplashScreen/index.tsx
+++ src/Screens/SplashScreen/index.tsx
@@ -1,5 +1,49 @@
-import { Text } from "react-native";
+import { useEffect, useState, useMemo } from 'react';
+import { ActivityIndicator } from "react-native";
+import { AppDescription, AppLogo, AppStart, AppStartText, AppTitle, Container } from "./styles";
+
+import { useAppTranslation } from "../../hooks/translation";
+import { CommonActions, useNavigation } from "@react-navigation/native";
+import { useProfile } from '../../hooks/profile';
+
+import AppLogoSource from '../../../assets/images/eating.png';
 
 export default function () {
-    return <Text>Splash</Text>
+  const { Translate } = useAppTranslation();
+  const [loading, setLoading] = useState(true);
+  const { profile } = useProfile();
+
+  const navigation = useNavigation();
+
+  function openMainScreenWithBottomTabs() {
+    navigation.dispatch(CommonActions.reset({
+      index: 1,
+      routes: [{ name: 'EntryPoint' }]
+    }));
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+
+      if (profile.createdAt !== null) openMainScreenWithBottomTabs();
+      else setLoading(false);
+
+    }, 1800);
+  }, []);
+
+  return (
+    <Container>
+      {loading ? <ActivityIndicator color="#FFF" size={25} /> : (
+        <>
+          <AppTitle>{Translate('App.Name')}</AppTitle>
+          <AppLogo source={AppLogoSource} />
+          <AppDescription>{Translate('App.Description')}</AppDescription>
+
+          <AppStart onPress={() => navigation.navigate('Settings')}>
+            <AppStartText>{Translate('Buttons.StartNow')}</AppStartText>
+          </AppStart>
+        </>
+      )}
+    </Container>
+  )
 }
