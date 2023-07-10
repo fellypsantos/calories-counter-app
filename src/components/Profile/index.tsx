63cc62fd976cc754d9f0a1c704a0d9d3a718e151
fellypsantos <fellypsantos2011@gmail.com>
2023-07-10 09:50:51 -0300

    profile component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,16 +1,19 @@
+import { useProfile } from "../../hooks/profile";
 import { Container, Name, Phrase, ProfilePicture, ProfilePictureContainer } from "./styles";
 
 const profilePictureMale = require('../../../assets/images/man.png');
 const profilePictureFemale = require('../../../assets/images/girl.png');
 
 export default function Profile() {
+  const { profile } = useProfile();
+
   return (
     <Container>
       <ProfilePictureContainer>
         <ProfilePicture source={profilePictureMale} />
       </ProfilePictureContainer>
-      <Name>Fellyp Santos</Name>
-      <Phrase>Fax o pix.</Phrase>
+      <Name>{profile.name}</Name>
+      <Phrase>{profile.phrase}</Phrase>
     </Container>
   );
 }
