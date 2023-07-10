72c88279dcc5e240dd57c20ef92b528fa9a8a4e9
fellypsantos <fellypsantos2011@gmail.com>
2023-07-10 09:51:52 -0300

    profile interface

---
+++ src/interfaces/IProfile.ts
@@ -0,0 +1,10 @@
+export interface IProfile {
+  name: string;
+  phrase: string;
+  weight: number;
+  height: number;
+  age: number;
+  gender: string;
+  activityFactor: number;
+  createdAt: Date;
+}
