92890716f318a8621d0780fe74418de553f8a792
fellypsantos <fellypsantos2011@gmail.com>
2023-07-10 09:52:14 -0300

    Settings page styles

---
+++ src/Tabs/Settings/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: #fff;
+`;
+
+export const MainView = styled.View`
+  flex: 1;
+  padding: 30px 20px;
+  padding-top: 40px;
+`;
+
+export const ButtonsContainer = styled.View`
+  margin-top: 25px;
+`;
