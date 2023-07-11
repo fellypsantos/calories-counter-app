77552ed3d11e5231dde0b3f03f49e326fc963a69
fellypsantos <fellypsantos2011@gmail.com>
2023-07-11 17:27:37 -0300

    Home component is now a StackNavigation

--- src/Tabs/Home/index.tsx
+++ src/Tabs/Home/index.tsx
@@ -1,52 +1,22 @@
-import { Linking } from "react-native";
-import ScrollViewContainer from "../../components/ScrollViewContainer";
+import { NativeStackNavigationProp, createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { BottomContainer, ListContainer, ListItemTouchable, TopContainer } from "./styles";
-import AdIconsBar from "../../components/AdIconsBar";
-import Profile from "../../components/Profile";
-import CaloriesCalculator from "../../components/CaloriesCalculator";
-import TopBarAddFoodRegistry from "../../components/TopBarAddFoodRegistry";
-import NoFoodRegistry from "../../components/NoFoodRegistry";
-import CallToAction from "../../components/CallToAction";
-import FoodRegistryListItem from "../../components/FoodRegistryListItem";
+import Dashboard from '../Dashboard';
+import AddFoodRegistry from '../../Screens/AddFoodRegistry';
 
-import { useAppTranslation } from '../../hooks/translation';
+export type RootStackParamList = {
+  Dashboard: undefined;
+  AddFoodRegistry: undefined;
+};
+
+export type StackNavigationProps = NativeStackNavigationProp<RootStackParamList>;
+
+const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 export default function Home() {
-
-  const { Translate } = useAppTranslation();
-
-  const theFoodHistory = [{ id: 1 }, { id: 2 }];
-
-  const isEmptyFoodList = theFoodHistory.length == 0;
-
   return (
-    <ScrollViewContainer>
-      <TopContainer>
-        <AdIconsBar />
-        <Profile />
-        <CaloriesCalculator />
-      </TopContainer>
-
-      <BottomContainer>
-        <TopBarAddFoodRegistry />
-        <NoFoodRegistry hidden={!isEmptyFoodList} />
-        <ListContainer>
-          {theFoodHistory.map(item => (
-            <ListItemTouchable key={item.id} onPress={() => null}>
-              <FoodRegistryListItem />
-            </ListItemTouchable>
-          ))}
-        </ListContainer>
-      </BottomContainer>
-
-      <CallToAction
-        title={Translate('BlockOption.Developer.MoreApps.Title')}
-        description={Translate('BlockOption.Developer.MoreApps.Description')}
-        handleOnPress={() => {
-          Linking.openURL('https://play.google.com/store/apps/dev?id=4983605265674024761');
-        }}
-      />
-    </ScrollViewContainer>
-  );
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName='Dashboard'>
+      <Screen name="Dashboard" component={Dashboard} />
+      <Screen name="AddFoodRegistry" component={AddFoodRegistry} />
+    </Navigator>
+  )
 }
