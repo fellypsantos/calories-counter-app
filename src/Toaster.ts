4c867f0687f185ef69dc5aab28fbf2fa33f65fc4
fellypsantos <fellypsantos2011@gmail.com>
2023-07-11 09:38:16 -0300

    added message position option

--- src/Toaster.ts
+++ src/Toaster.ts
@@ -1,18 +1,19 @@
-import { ToastAndroid } from 'react-native';
+import { ToastAndroid, ToastAndroidStatic } from 'react-native';
 
 interface IToastProps {
   text: string;
   duration?: 'SHORT' | 'LONG';
+  position?: 'BOTTOM' | 'CENTER',
 }
 
 export default {
   ShowToast: (props: IToastProps) => {
-    const { text, duration = 'SHORT' } = props;
+    const { text, duration = 'SHORT', position = 'BOTTOM' } = props;
 
     ToastAndroid.showWithGravityAndOffset(
       text,
       duration === 'SHORT' ? ToastAndroid.SHORT : ToastAndroid.LONG,
-      ToastAndroid.BOTTOM,
+      position === 'BOTTOM' ? ToastAndroid.BOTTOM : ToastAndroid.CENTER,
       0,
       160,
     );
