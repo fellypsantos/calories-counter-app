d3cb9592d6f7301ae5fa9d21b9c8cf1eb3c31a80
fellypsantos <fellypsantos2011@gmail.com>
2023-07-11 20:37:36 -0300

    fix color text in subtitle component

--- src/components/Subtitle/styles.ts
+++ src/components/Subtitle/styles.ts
@@ -4,5 +4,5 @@ import Colors from '../../Colors';
 export const CustomSubtitle = styled.Text`
   font-family: 'Open Sans Regular';
   font-size: 15px;
-  color: ${Colors.Primary};
+  color: ${Colors.TextPrimary};
 `;
