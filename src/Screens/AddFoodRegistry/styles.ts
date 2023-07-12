07676b7b1d6ca9bd33512b340782ac29e081bec6
fellypsantos <fellypsantos2011@gmail.com>
2023-07-12 12:48:26 -0300

    fix UI

--- src/Screens/AddFoodRegistry/styles.ts
+++ src/Screens/AddFoodRegistry/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView`
-  flex: 1;
-`;
+export const Container = styled.ScrollView``;
 
 export const AppSection = styled.View`
   flex: 1;
