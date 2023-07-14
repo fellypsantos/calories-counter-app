0e5048dbe5e9755a5c03b22ca1156df2fe273000
fellypsantos <fellypsantos2011@gmail.com>
2023-07-14 11:13:52 -0300

    added dynamic icons and message to food record

--- src/components/FoodRegistryListItem/index.tsx
+++ src/components/FoodRegistryListItem/index.tsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react';
 import dayjs from "dayjs";
 import Colors from "../../Colors";
 import { IFoodRecord } from "../../interfaces/IFoodRecord";
 import { Container, FoodInfo, FoodName, IconContainer, IconImage, RegistryDataContainer } from "./styles";
 import Icon from '@expo/vector-icons/FontAwesome5';
 import { useAppTranslation } from "../../hooks/translation";
+import { ImageSourcePropType } from 'react-native';
 
 const localizedFormat = require('dayjs/plugin/localizedFormat');
 dayjs.extend(localizedFormat);
@@ -20,23 +22,46 @@ interface IProps {
 
 export default function FoodRegistryListItem({ foodRecord }: IProps) {
 
-  const { selectedLanguage } = useAppTranslation();
+  const { Translate, selectedLanguage } = useAppTranslation();
 
   const { id, name, kcal, categoryLevel, timestamp } = foodRecord;
+
+  const categoryLevelMessage = useMemo(() => {
+
+    if (categoryLevel === 1) return Translate('Food.Light');
+    if (categoryLevel === 2) return Translate('Food.Moderate');
+    if (categoryLevel === 3) return Translate('Food.Heavy');
+  }, [foodRecord, selectedLanguage]);
+
+  const categoryLevelIcon = useMemo(() => {
+
+    if (categoryLevel === 1) return 'smile';
+    if (categoryLevel === 2) return 'exclamation-triangle';
+    if (categoryLevel === 3) return 'sad-tear';
+  }, [foodRecord]);
+
+  const timestampIcon = useMemo<ImageSourcePropType>(() => {
+
+    const time = parseInt(dayjs(timestamp).format('HH'), 10);
+
+    if (time >= 6 && time < 12) return TimeIcons.sunrise;
+    if (time >= 12 && time < 18) return TimeIcons.sun;
+    if (time >= 18 && time <= 23 || time >= 0 && time < 6) return TimeIcons.moon;
+  }, [foodRecord]);
 
   return (
     <Container>
       <IconContainer>
-        <IconImage source={TimeIcons['sunrise']} />
+        <IconImage source={timestampIcon} />
       </IconContainer>
 
       <RegistryDataContainer>
         <FoodName>{name}</FoodName>
-        <FoodInfo>Alimentação Pesada | {dayjs(timestamp).locale(selectedLanguage).format('LT')}</FoodInfo>
+        <FoodInfo>{categoryLevelMessage} | {dayjs(timestamp).locale(selectedLanguage).format('LT')}</FoodInfo>
         <FoodInfo>{kcal} kcal</FoodInfo>
       </RegistryDataContainer>
 
-      <Icon name='smile' size={20} color={Colors.Primary} />
+      <Icon name={categoryLevelIcon} size={20} color={Colors.Primary} />
     </Container>
   )
 }
