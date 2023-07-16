bf6f8fac78499eb48460bc3f98b12d14772e02ec
fellypsantos <fellypsantos2011@gmail.com>
2023-07-16 11:25:24 -0300

    added missing routes

--- @types/navigation.d.ts
+++ @types/navigation.d.ts
@@ -1,9 +1,16 @@
+import { IFoodRecord } from "../src/interfaces/IFoodRecord";
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       AdMobRewardIntro: undefined;
-      AddFoodRegistry: undefined;
-      Settings: undefined;
+      Dashboard: undefined;
+      AddFoodRegistry: { foodRecord: IFoodRecord | undefined };
+      SplashScreen: undefined;
+      EntryPoint: undefined;
+      Home: undefined;
+      History: undefined;
+      Settings: undefined
     }
   }
 }
