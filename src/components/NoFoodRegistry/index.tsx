304f21147d0dabd67b0bcd05c100ed313b85128d
fellypsantos <fellypsantos2011@gmail.com>
2023-07-16 20:44:37 -0300

    applied missing translation

--- src/components/NoFoodRegistry/index.tsx
+++ src/components/NoFoodRegistry/index.tsx
@@ -2,12 +2,14 @@ import Icon from '@expo/vector-icons/FontAwesome5';
 import { Container, NoRegistryLabel } from "./styles";
 
 import Colors from '../../Colors';
+import { useAppTranslation } from '../../hooks/translation';
 
 export default function NoFoodRegistry({ hidden }: IVisibility) {
+  const { Translate } = useAppTranslation();
   return (
     <Container hidden={hidden}>
       <Icon name="mug-hot" size={30} color={Colors.TextPrimary} />
-      <NoRegistryLabel>Nenhum registro</NoRegistryLabel>
+      <NoRegistryLabel>{Translate('NoRecordsFound')}</NoRegistryLabel>
     </Container>
   );
 }
