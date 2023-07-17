a384ee1941d5220084d60da5317a65f91a71161a
fellypsantos <fellypsantos2011@gmail.com>
2023-07-17 13:07:03 -0300

    aditional props to Subtitle component

--- src/components/Subtitle/index.tsx
+++ src/components/Subtitle/index.tsx
@@ -3,8 +3,14 @@ import { CustomSubtitle } from './styles';
 
 interface IProps {
   children: React.ReactNode,
+  renderAsParagraph?: boolean,
+  useMarginTop?: boolean,
 }
 
-const Subtitle = ({ children }: IProps) => <CustomSubtitle>{children}</CustomSubtitle>;
+const Subtitle = ({ children, renderAsParagraph, useMarginTop }: IProps) => (
+  <CustomSubtitle
+    renderAsParagraph={renderAsParagraph}
+    useMarginTop={useMarginTop}>{children}</CustomSubtitle>
+)
 
 export default Subtitle;
