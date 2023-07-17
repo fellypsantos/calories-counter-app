e1dc365c2ab8a8eac5c7b1e34bbe0704a270f3df
fellypsantos <fellypsantos2011@gmail.com>
2023-07-17 18:39:26 -0300

    translated curiosity screen content

--- src/Screens/CuriosityContent/index.tsx
+++ src/Screens/CuriosityContent/index.tsx
@@ -1,15 +1,21 @@
 import MainTitle from "../../components/MainTitle";
 import Subtitle from "../../components/Subtitle";
 import Icon from '@expo/vector-icons/FontAwesome5';
+import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
 
 import Colors from "../../Colors";
 import SmallBoxContent from "../../components/SmallBoxContent";
 import { useNavigation } from "@react-navigation/native";
 
-import { Scrollable, Container, CloseIconBox } from "./styles";
+import { Scrollable, Container, CloseIconBox, NativeBannerContainer } from "./styles";
+import { useAppTranslation } from "../../hooks/translation";
+import AdUnits from "../../AdUnits";
+import { usePremium } from "../../hooks/premium";
 
 export default function CuriosityContent() {
 
+  const { Translate } = useAppTranslation();
+  const { isPremiumTime } = usePremium();
   const navigation = useNavigation();
 
   return (
@@ -18,18 +24,26 @@ export default function CuriosityContent() {
         <CloseIconBox onPress={() => navigation.goBack()}>
           <Icon name="times-circle" size={20} color="#666" />
         </CloseIconBox>
+
         <Icon name="fire" size={40} color={Colors.Primary} />
-        <MainTitle>What is basal energy?</MainTitle>
+        <MainTitle>{Translate('Curiosity.BasalEnergy.Title')}</MainTitle>
+        <Subtitle renderAsParagraph>{'\t\t\t'}{Translate('Curiosity.BasalEnergy.Paragraph1')}</Subtitle>
 
-        <Subtitle renderAsParagraph>{'\t\t\t'}Basal energy is the amount of energy needed to sustain all the basic functions of a living organism. This energy requirement is most accurate when describing the needs of an alert, resting human in a calm mental and physical state and in favorable temperature conditions.</Subtitle>
+        {
+          !isPremiumTime && (
+            <NativeBannerContainer>
+              <BannerAd unitId={AdUnits.BannerMainFooter} size={BannerAdSize.MEDIUM_RECTANGLE} />
+            </NativeBannerContainer>
+          )
+        }
 
-        <MainTitle>How to calculate?</MainTitle>
+        <MainTitle>{Translate('Curiosity.HowToCalculate.Title')}</MainTitle>
+        <Subtitle renderAsParagraph>{'\t\t\t'}{Translate('Curiosity.HowToCalculate.Paragraph1')}</Subtitle>
+        <SmallBoxContent title={Translate('Curiosity.HowToCalculate.Male.Title')} content={Translate('Curiosity.HowToCalculate.Male.Formula')} />
+        <SmallBoxContent title={Translate('Curiosity.HowToCalculate.Female.Title')} content={Translate('Curiosity.HowToCalculate.Female.Formula')} />
 
-        <SmallBoxContent title="Male" content="66.5 + (13.75 * weight) + (5.003 * height) - (6.76 * age)" />
-        <SmallBoxContent title="Female" content="65.51 + (13.75 * weight) + (5.003 * height) - (6.775 * age)" />
-
-        <MainTitle>How is basal metabolism related to calories?</MainTitle>
-        <Subtitle renderAsParagraph>{'\t\t\t'}The relation between basal metabolism and calorie intake is pretty simple; the higher the basal metabolism/energy expenditure, the higher the number of calories you should consume. In order words, if you need to lose weight (or be able to eat more without gaining weight 🍔), you need to increase your basal metabolism.</Subtitle>
+        <MainTitle>{Translate('Curiosity.BasalMetabolismAndCalories.Title')}</MainTitle>
+        <Subtitle renderAsParagraph>{'\t\t\t'}{Translate('Curiosity.BasalMetabolismAndCalories.Paragraph1')}</Subtitle>
       </Container>
     </Scrollable>
   );
