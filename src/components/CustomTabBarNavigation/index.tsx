4a3a4d462c2ae8804f5018cb2405df158a185721
fellypsantos <fellypsantos2011@gmail.com>
2023-07-16 23:40:16 -0300

    hide footer banner when premium time is active

--- src/components/CustomTabBarNavigation/index.tsx
+++ src/components/CustomTabBarNavigation/index.tsx
@@ -1,13 +1,20 @@
 import { View } from 'react-native';
-import { TestIds, BannerAdSize, BannerAd } from 'react-native-google-mobile-ads';
+import { BannerAdSize, BannerAd } from 'react-native-google-mobile-ads';
 import { BottomTabBar, BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { usePremium } from '../../hooks/premium';
+import AdUnits from '../../AdUnits';
+
 
 export default function (props: BottomTabBarProps) {
 
-    return (
-        <View>
-            <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
-            <BottomTabBar {...props} />
-        </View>
-    )
+  const { isPremiumTime } = usePremium();
+
+  return (
+    <View>
+      {
+        !isPremiumTime && <BannerAd unitId={AdUnits.BannerMainFooter} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
+      }
+      <BottomTabBar {...props} />
+    </View>
+  )
 }
