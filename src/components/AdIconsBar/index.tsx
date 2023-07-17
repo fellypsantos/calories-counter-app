1d97b7ce00f031911666735a0adb865b27bf2035
fellypsantos <fellypsantos2011@gmail.com>
2023-07-16 23:39:42 -0300

    hide gift icon when premium time is active

--- src/components/AdIconsBar/index.tsx
+++ src/components/AdIconsBar/index.tsx
@@ -2,10 +2,11 @@ import { TouchableOpacity } from "react-native";
 import { AdButtonBlock, AdButtonContainer } from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import Icon from '@expo/vector-icons/FontAwesome5';
+import { usePremium } from "../../hooks/premium";
 
 export default function AdIconsBar() {
   const navigation = useNavigation();
-  const isPremiumTime = false;
+  const { isPremiumTime } = usePremium();
 
   return (
     <AdButtonContainer>
