b1b463aa9f67d723d1af1201a7df719086d30a75
fellypsantos <fellypsantos2011@gmail.com>
2023-07-19 18:54:39 -0300

    handle update steps in fixed bottom tab bar

--- src/components/CustomTabBarNavigation/index.tsx
+++ src/components/CustomTabBarNavigation/index.tsx
@@ -2,18 +2,46 @@ import { View } from 'react-native';
 import { BannerAdSize, BannerAd } from 'react-native-google-mobile-ads';
 import { BottomTabBar, BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { usePremium } from '../../hooks/premium';
-import AdUnits from '../../AdUnits';
+import { useUpdater } from '../../hooks/updater';
+import { useAppTranslation } from '../../hooks/translation';
 
+import ButtonStrech from '../ButtonStretch';
+import ProgressIndicatorText from '../ProgressIndicatorText';
+import AdUnits from '../../AdUnits';
 
 export default function (props: BottomTabBarProps) {
 
   const { isPremiumTime } = usePremium();
+  const { Translate } = useAppTranslation();
+
+  const {
+    updateButtonVisible,
+    installDownloadedUpdate,
+    downloadProgress
+  } = useUpdater();
 
   return (
     <View>
+      {downloadProgress > 0 && <ProgressIndicatorText value={downloadProgress} />}
+
       {
-        !isPremiumTime && <BannerAd unitId={AdUnits.BannerMainFooter} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
+        updateButtonVisible && (
+          <ButtonStrech
+            isBlinking
+            text={Translate('Updater.InstallUpdate')}
+            onPress={installDownloadedUpdate} />
+        )
       }
+
+      {
+        !isPremiumTime && (
+          <BannerAd
+            unitId={AdUnits.BannerMainFooter}
+            size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          />
+        )
+      }
+
       <BottomTabBar {...props} />
     </View>
   )
