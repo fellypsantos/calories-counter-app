2fca5198f24651f977305cc57ec2b904c859e5ae
fellypsantos <fellypsantos2011@gmail.com>
2023-07-19 18:41:39 -0300

    updated interface with extracted interface

--- src/components/ButtonDefault/index.tsx
+++ src/components/ButtonDefault/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
+import { IButton } from '../../interfaces/IButton';
+
 import { Container, IconBox, Label } from './styles';
 
-interface IProps {
-  text: string;
-  onPress(): void;
-}
-
-const ButtonDefault = ({ text = '', onPress }: IProps) => (
+const ButtonDefault = ({ text = '', onPress }: IButton) => (
   <Container onPress={onPress}>
     <>
       <IconBox name="check" size={18} />
