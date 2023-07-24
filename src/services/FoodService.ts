f1544c0b622cf38b21fb89c8afe4ddc2b22fe87b
fellypsantos <fellypsantos2011@gmail.com>
2023-07-24 09:26:37 -0300

    handle food operations using repository

---
+++ src/services/FoodService.ts
@@ -0,0 +1,16 @@
+import DataBase from "../databases";
+import { IFoodRecord } from "../interfaces/IFoodRecord";
+import IFoodRepository from "../interfaces/IFoodRepository";
+import FoodRepository from "../repositories/FoodRepository";
+
+export default class FoodService {
+  private foodRepository: IFoodRepository;
+
+  constructor() {
+    this.foodRepository = new FoodRepository(DataBase.db);
+  }
+
+  async getFoodHistory(dateISO8061: string): Promise<IFoodRecord[]> {
+    return await this.foodRepository.getFoodHistory(dateISO8061);
+  }
+}
