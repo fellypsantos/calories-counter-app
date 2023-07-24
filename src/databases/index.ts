0de107a257b153e9870277d6d1dd1d710f3e53d1
fellypsantos <fellypsantos2011@gmail.com>
2023-07-23 22:33:43 -0300

    methods moved to ProfileRepository

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -5,70 +5,24 @@ import { IFoodRecord } from '../interfaces/IFoodRecord';
 export default class DataBase {
   static db: SQLiteDatabase;
 
-  static open = () => {
-    if (!this.db) {
-      this.db = SQLite.openDatabase({ name: 'appDB', createFromLocation: '~database.sqlite' },
-        () => { console.log('DATABASE SUCCESSFULLY OPENED!'); },
-      );
-    }
-    return this.db;
+  static open = (): Promise<SQLiteDatabase> => {
+    return new Promise((resolve, reject) => {
+      if (!this.db) {
+        this.db = SQLite.openDatabase({ name: 'appDB', createFromLocation: '~database.sqlite' },
+          () => {
+            if (__DEV__) console.log('DATABASE SUCCESSFULLY OPENED!');
+            resolve(this.db);
+          },
+          error => {
+            reject(error.message);
+          }
+        );
+      }
+    });
   };
 
   static validateConnection = () => {
     if (!this.db) throw new Error("Database connection is closed.");
-  };
-
-  static getProfileData = (callback: (result: IProfile | null) => void) => {
-    this.validateConnection();
-
-    this.db.transaction(tx => {
-      tx.executeSql('SELECT * FROM profile', undefined, (_, results) => {
-        if (results.rows.length === 0) {
-          callback(null);
-          return;
-        }
-
-        const profile: IProfile = results.rows.item(0);
-        callback(profile);
-      });
-    });
-  };
-
-  static addProfile = (profile: IProfile, callback: (success: boolean) => void) => {
-    this.validateConnection();
-
-    const {
-      name,
-      phrase,
-      weight,
-      height,
-      age,
-      gender,
-      language,
-      activityFactor,
-      createdAt
-    } = profile;
-
-    const sqlToRun = 'INSERT INTO profile(name, phrase, weight, height, age, gender, language, activityFactor, createdAt) VALUES (?,?,?,?,?,?,?,?,?)';
-
-    const sqlValues = [
-      name,
-      phrase,
-      weight,
-      height,
-      age,
-      gender,
-      language,
-      activityFactor,
-      createdAt
-    ];
-
-    this.db.transaction(tx => {
-      tx.executeSql(sqlToRun, sqlValues,
-        (_, results) => {
-          callback(results.rowsAffected > 0);
-        });
-    });
   };
 
   static updateProfile = (profile: IProfile, callback: (success: boolean) => void) => {
