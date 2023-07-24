e005f4a9e905ba8a6cf6d9c9a3067b860b0205f0
fellypsantos <fellypsantos2011@gmail.com>
2023-07-24 10:24:30 -0300

    handle the activation for premium time

---
+++ src/services/PremiumService.ts
@@ -0,0 +1,15 @@
+import DataBase from "../databases";
+import IPremiumRepository from "../interfaces/IPremiumRepository";
+import PremiumRepository from "../repositories/PremiumRepository";
+
+export default class PremiumService {
+  private premiumRepository: IPremiumRepository;
+
+  constructor() {
+    this.premiumRepository = new PremiumRepository(DataBase.db);
+  }
+
+  async setLastPremiumTimestamp(timestamp: string): Promise<boolean> {
+    return await this.premiumRepository.setLastPremiumTimestamp(timestamp);
+  }
+}
